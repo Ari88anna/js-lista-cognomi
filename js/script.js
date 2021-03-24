fe0ed40ce6a97b50fbf7358041c088dfa05b4cb6
Arianna Carotti <arianna.carotti@gmail.com>
2021-03-24 15:43:36 +0100

    cominciato esrcizio

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1)
+// della lista in cui il nuovo utente si trova
+// (facciamo questo in 2 versioni: col ciclo for e col while)
+
+
+// chiediamo il cognome all'utente
+
+var cognomeUtente = prompt ('Qual è il tuo cognome?');
+
+// creiamo una lista di cognomi
+
+var  cognomiLista = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli' ];
+console.log(cognomiLista);
+
+// poi inseriamo il cognome dell'utente alla lista
+
+cognomiLista.push (cognomeUtente);
+console.log(cognomiLista);
+
+
+// stampare la lista in ordine alfabetico
+
+cognomiLista.sort();
+console.log(cognomiLista)
+
+
+for ( var i = 0; i<cognomiLista.length; i++ ) {
+    console.log(cognomiLista[i])
+    
+    if (cognomiLista[i] == cognomeUtente) {
+        console.log(i)
+    }
+}
+
+// scrivere la posizione umana della lista in cui il nuovo utente si trova
+
